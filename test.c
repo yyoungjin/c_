d2db7bcd21b85180fe05604e91c37ab992ef46b1
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-17 22:35:49 +0900

    332p Q2

--- test.c
+++ test.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-int GetData(void *p_data, char type)
+int GetData(void *p_data)
 {
-    int result = 0;
-    if (type == 1) result = *(char *)p_data;
-    else if (type == 2) result = *(short *)p_data;
-    else if (type == 4) result = *(int *)p_data;
-    return result;
+    *(short *)p_data = 0x0412;
 }
 
 
 void main()
 {
     int data = 0x12345678;
-    printf("%X\n", GetData(&data, 2));
+    GetData(&data);
+    printf("%X\n", data);
 }
