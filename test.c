84da3d79a1ebc9dc5e4bbc342a420ccaad29cebe
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-18 15:29:43 +0900

    340p

--- test.c
+++ test.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-int GetData(void *p_data)
-{
-    *(short *)p_data = 0x0412;
-}
-
-
 void main()
 {
-    int data = 0x12345678;
-    GetData(&data);
-    printf("%X\n", data);
+    int input_data = getchar();
+    rewind(stdin);
+    printf("first input : %c\n", input_data);
+    input_data = getchar();
+    rewind(stdin);
+    printf("second input : %c\n", input_data);
 }
