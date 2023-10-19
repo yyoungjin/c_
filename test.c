4de2a6084d3dd5a4a0f044d8797cbda3d12b07a0
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-19 15:18:28 +0900

    357p

--- test.c
+++ test.c
@@ -1,27 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-int GetMyString(char buffer[ ], int limit)
-{
-    int i;
-    for(i = 0; i<limit; i++) {
-        buffer[i] = getchar();
-        if(buffer[i] == '\n') {
-            buffer[i] = 0;
-            return 1;
-        }
-    }
-    buffer[i] = 0;
-    rewind(stdin);
-    return 0;
-}ㅡ
-
 void main()
 {
-    char temp[10];
-    int state;
-    state = GetMyString(temp, 9);
+    int int_data;
+    float float_data;
 
-    if(state == 1) printf("input : %s\n", temp);
-    else printf("input : %s -> out of range\n", temp);
+    scanf("%d", &int_data);
+    scanf("%f", &float_data);
+
+    printf("input : %d, %f\n", int_data, float_data);
 }
