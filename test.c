04503e209bbe073feebacf0293414c983fcc27db
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-19 14:36:36 +0900

    346p

--- test.c
+++ test.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+
+int GetMyString(char buffer[ ], int limit)
+{
+    int i;
+    for(i = 0; i<limit; i++) {
+        buffer[i] = getchar();
+        if(buffer[i] == '\n') {
+            buffer[i] = 0;
+            return 1;
+        }
+    }
+    buffer[i] = 0;
+    rewind(stdin);
+    return 0;
+}ㅡ
+
 void main()
 {
-    int input_data = getchar();
-    rewind(stdin);
-    printf("first input : %c\n", input_data);
-    input_data = getchar();
-    rewind(stdin);
-    printf("second input : %c\n", input_data);
+    char temp[10];
+    int state;
+    state = GetMyString(temp, 9);
+
+    if(state == 1) printf("input : %s\n", temp);
+    else printf("input : %s -> out of range\n", temp);
 }
