52f0b215450796a57915db2702c95d6952c141c5
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-21 11:44:16 +0900

    364p

--- test.c
+++ test.c
@@ -3,6 +3,17 @@
 
 void main()
 {
-    int num1, num2;
-    scanf("%d %d", &num1, &num2);
+    int num = 0;
+    while(1) {
+        printf("input age : ");
+        scanf("%d", &num);
+
+        if(num > 0 && num <= 130) {
+            break;
+        }
+        else {
+            printf("Incorrect Age! \n");
+        }
+    }
+    printf("your age : %d \n", num);
 }
