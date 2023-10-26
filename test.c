fcd908795a72d1ed8d9c132d1d145c00572cc649
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-26 22:29:00 +0900

    419p

--- test.c
+++ test.c
@@ -4,15 +4,26 @@
 
 int main()
 {
-    char *p_name;
-    p_name = (char *)malloc(32);
-    if (p_name != NULL) {
-        printf("your name : ");
-        gets(p_name);
-        printf("Hi~ %s\n", p_name);
-        free(p_name);
-    } else {
-        printf("Memory allocation error!");
+    int *p_num_list, count = 0, sum = 0, limit = 0, i;
+
+    printf("사용할 최대 개수를 입력하세요 : ");
+    scanf("%d", &limit);
+
+    p_num_list = (int *)malloc(sizeof(int)*limit);
+
+    while(count < limit) {
+        printf("숫자를 입력하세요 (9999를 누르면 종료) : ");
+        scanf("%d", p_num_list + count);
+        if(*(p_num_list + count) == 9999) break;
+        count++;
     }
+
+    for(i = 0; i < count; i++) {
+        if(i > 0) printf(" + ");
+        printf(" %d ", *(p_num_list + i));
+        sum = sum + *(p_num_list + i);
+    }
+    printf(" = %d\n", sum);
+    free(p_num_list);
     return 0;
 } 
