9b8ec4f20b3dba87b912bdc83d7008f7be7b4384
노영진 <133024268+yyoungjin@users.noreply.github.com>
2023-10-27 10:15:23 +0900

    429p

--- test.c
+++ test.c
@@ -4,26 +4,14 @@
 
 int main()
 {
-    int *p_num_list, count = 0, sum = 0, limit = 0, i;
+    short data = 3;
+    short *p = &data;
+    short **pp = &p;
 
-    printf("사용할 최대 개수를 입력하세요 : ");
-    scanf("%d", &limit);
-
-    p_num_list = (int *)malloc(sizeof(int)*limit);
-
-    while(count < limit) {
-        printf("숫자를 입력하세요 (9999를 누르면 종료) : ");
-        scanf("%d", p_num_list + count);
-        if(*(p_num_list + count) == 9999) break;
-        count++;
-    }
-
-    for(i = 0; i < count; i++) {
-        if(i > 0) printf(" + ");
-        printf(" %d ", *(p_num_list + i));
-        sum = sum + *(p_num_list + i);
-    }
-    printf(" = %d\n", sum);
-    free(p_num_list);
+    printf("Before data : %d\n", data);
+    *p = 4;
+    printf("Use *p data : %d\n", data);
+    **pp = 5;
+    printf("Use **pp data : %d\n", data);
     return 0;
 } 
